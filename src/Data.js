5a2ce94e4a6ee7ab7e563561e90b0b6e5435ce36
zakaria-bouchatal <bouchatal960@gmail.com>
2023-09-21 22:51:58 +0000

    the challenge number 11

---
+++ src/Data.js
@@ -0,0 +1,73 @@
+let shopItemsData = [
+  {
+    id: "ioytrhndcv",
+    name: "macbook",
+    price: 1000,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/1.jpg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "old ahh computer",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/2.jpg"
+  },
+  {
+    id: "thyfhcbcv",
+    name: "samsung phone",
+    price: 180,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/3.webp",
+  },
+  {
+    id: "thyfhcbch",
+    name: "tablet",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/4.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "nintendo switch",
+    price: 700,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/5.webp",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "wireless speaker",
+    price: 70,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/6.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "smart watch",
+    price: 250,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/7.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "VR",
+    price: 350,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/8.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "earbuds",
+    price: 150,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/9.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "XBOX",
+    price: 710,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/10.jpg",
+  },
+
+];
